a934d9822d99e31524ef14352ee22ba36b75435a
Alan <Alan@DESKTOP-TG930HR>
2024-02-14 13:20:43 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Form_ejercicio1_estudiantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Matricula = new System.Windows.Forms.TextBox();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.txt_Apellidos = new System.Windows.Forms.TextBox();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.txt_Edad = new System.Windows.Forms.TextBox();
+            this.lbl_5 = new System.Windows.Forms.Label();
+            this.txt_Carrera = new System.Windows.Forms.TextBox();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.lbl_Imprimir = new System.Windows.Forms.Label();
+            this.btn_Imprimir = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.lbl_Prueba = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_Matricula
+            // 
+            this.txt_Matricula.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Matricula.Location = new System.Drawing.Point(178, 53);
+            this.txt_Matricula.Name = "txt_Matricula";
+            this.txt_Matricula.Size = new System.Drawing.Size(174, 28);
+            this.txt_Matricula.TabIndex = 0;
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.Location = new System.Drawing.Point(39, 53);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(77, 20);
+            this.lbl_1.TabIndex = 1;
+            this.lbl_1.Text = "Matricula";
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_3.Location = new System.Drawing.Point(39, 146);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(75, 20);
+            this.lbl_3.TabIndex = 5;
+            this.lbl_3.Text = "Apellidos";
+            // 
+            // txt_Apellidos
+            // 
+            this.txt_Apellidos.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Apellidos.Location = new System.Drawing.Point(178, 146);
+            this.txt_Apellidos.Name = "txt_Apellidos";
+            this.txt_Apellidos.Size = new System.Drawing.Size(174, 28);
+            this.txt_Apellidos.TabIndex = 4;
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_4.Location = new System.Drawing.Point(39, 194);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(46, 20);
+            this.lbl_4.TabIndex = 7;
+            this.lbl_4.Text = "Edad";
+            // 
+            // txt_Edad
+            // 
+            this.txt_Edad.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Edad.Location = new System.Drawing.Point(178, 194);
+            this.txt_Edad.Name = "txt_Edad";
+            this.txt_Edad.Size = new System.Drawing.Size(174, 28);
+            this.txt_Edad.TabIndex = 6;
+            // 
+            // lbl_5
+            // 
+            this.lbl_5.AutoSize = true;
+            this.lbl_5.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_5.Location = new System.Drawing.Point(39, 236);
+            this.lbl_5.Name = "lbl_5";
+            this.lbl_5.Size = new System.Drawing.Size(63, 20);
+            this.lbl_5.TabIndex = 9;
+            this.lbl_5.Text = "Carrera";
+            // 
+            // txt_Carrera
+            // 
+            this.txt_Carrera.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Carrera.Location = new System.Drawing.Point(178, 236);
+            this.txt_Carrera.Name = "txt_Carrera";
+            this.txt_Carrera.Size = new System.Drawing.Size(174, 28);
+            this.txt_Carrera.TabIndex = 8;
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_2.Location = new System.Drawing.Point(39, 101);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(68, 20);
+            this.lbl_2.TabIndex = 11;
+            this.lbl_2.Text = "Nombre";
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Nombre.Location = new System.Drawing.Point(178, 101);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(174, 28);
+            this.txt_Nombre.TabIndex = 10;
+            // 
+            // lbl_Imprimir
+            // 
+            this.lbl_Imprimir.AutoSize = true;
+            this.lbl_Imprimir.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Imprimir.Location = new System.Drawing.Point(607, 70);
+            this.lbl_Imprimir.Name = "lbl_Imprimir";
+            this.lbl_Imprimir.Size = new System.Drawing.Size(0, 20);
+            this.lbl_Imprimir.TabIndex = 12;
+            // 
+            // btn_Imprimir
+            // 
+            this.btn_Imprimir.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Imprimir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Imprimir.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Imprimir.Location = new System.Drawing.Point(399, 70);
+            this.btn_Imprimir.Name = "btn_Imprimir";
+            this.btn_Imprimir.Size = new System.Drawing.Size(155, 51);
+            this.btn_Imprimir.TabIndex = 13;
+            this.btn_Imprimir.Text = "Imprimir";
+            this.btn_Imprimir.UseVisualStyleBackColor = false;
+            this.btn_Imprimir.Click += new System.EventHandler(this.btn_Imprimir_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Limpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(399, 127);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(155, 51);
+            this.btn_Limpiar.TabIndex = 14;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Cerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Cerrar.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cerrar.Location = new System.Drawing.Point(399, 185);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(155, 51);
+            this.btn_Cerrar.TabIndex = 15;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // lbl_Prueba
+            // 
+            this.lbl_Prueba.AutoSize = true;
+            this.lbl_Prueba.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Prueba.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Prueba.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Prueba.Location = new System.Drawing.Point(670, 146);
+            this.lbl_Prueba.Name = "lbl_Prueba";
+            this.lbl_Prueba.Size = new System.Drawing.Size(95, 20);
+            this.lbl_Prueba.TabIndex = 16;
+            this.lbl_Prueba.Text = "Resultado...";
+            this.lbl_Prueba.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 450);
+            this.Controls.Add(this.lbl_Prueba);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Imprimir);
+            this.Controls.Add(this.lbl_Imprimir);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.lbl_5);
+            this.Controls.Add(this.txt_Carrera);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.txt_Edad);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.txt_Apellidos);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.txt_Matricula);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Matricula;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.TextBox txt_Apellidos;
+        private System.Windows.Forms.Label lbl_4;
+        private System.Windows.Forms.TextBox txt_Edad;
+        private System.Windows.Forms.Label lbl_5;
+        private System.Windows.Forms.TextBox txt_Carrera;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.Label lbl_Imprimir;
+        private System.Windows.Forms.Button btn_Imprimir;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.Label lbl_Prueba;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_ejercicio1_estudiantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Imprimir_Click(object sender, EventArgs e)
+        {
+            string nombre, apellido, carrera;
+
+            int matricula, edad;
+
+            nombre = txt_Nombre.Text;
+            apellido = txt_Apellidos.Text;
+            carrera = txt_Carrera.Text;
+
+            matricula = Convert.ToInt32(txt_Matricula.Text);
+            edad = Convert.ToInt32(txt_Edad.Text);
+            
+
+           string Datos = $"Matricula :   {matricula}" + " " + $"Nombre    :   {nombre}" + " " + $"Apellido  :   {apellido}" + " " + $"Edad      :   {edad} " + " " + $"Carrera   :   {carrera}";
+
+
+            lbl_Prueba.Text = Datos;
+
+
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_Apellidos.Text = string.Empty;
+            txt_Carrera.Text = string.Empty;
+            txt_Edad.Text = string.Empty;
+            txt_Matricula.Text = string.Empty;
+            txt_Nombre.Text = string.Empty;
+            lbl_Imprimir.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
